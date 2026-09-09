d8212fc3260802e5a902ecc1182e66579defb613
CasperLCasper <64712311+CasperLCasper@users.noreply.github.com>
2026-09-09 13:11:50 +0300

    Update w47.js

--- public/w47.js
+++ public/w47.js
@@ -5,7 +5,7 @@ o9
 uyyfuyveruyubihnuyf7668b7tb76br7 6
 ut4aw7654w6
      9 4nqyt7r6 5dieuejiueshkjzoesrjioesiong 8w347 3]=09-0484y9
- s g grehshjk-=
+ s g grehshjk-=uyguyfuyuy
    iuuybyvddv gwey63t 63 
 uygttybfdhfhn uyguguyguyg
     hruhriuheuiapo[ao[ao-094ue09uq2 0u   g78  r42e273yu
